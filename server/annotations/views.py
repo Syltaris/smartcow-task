78b1befb926f27f9a238afbe0e27c6dbf416f51b
zexuan <syltaris@gmail.com>
2021-12-07 22:11:45 +0800

    setup views

--- server/annotations/views.py
+++ server/annotations/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.serializers import Serializer, PrimaryKeyRelatedField
+from rest_framework.response import Response
+
+from annotations.models import Annotation, Project, Image
+
+
+class AnnotationSerializer(Serializer):
+    class Meta:
+        model = Annotation
+
+
+class ProjectSerializer(Serializer):
+
+    images = PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ("id", "images")
+
+
+class ImageSerializer(Serializer):
+    annotations = PrimaryKeyRelatedField(queryset=Image.objects.all(), many=True)
+
+    class Meta:
+        model = Image
+        fields = ("annotations", "url")
+
+    def to_url(self, obj):
+        return obj.image.url
+
 
 # Create your views here.
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+
+    def list(self, request):
+        serializer = ProjectSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+# Create your views here.
+class ImageViewSet(ModelViewSet):
+    queryset = Image.objects.all()
+
+    def list(self, request):
+        serializer = ImageSerializer(self.queryset, many=True)
+        return Response(serializer.data)
--- server/server/urls.py
+++ server/server/urls.py
@@ -16,6 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import routers
+
+from annotations.views import ProjectViewSet, ImageViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r"projects", ProjectViewSet)
+router.register(r"images", ImageViewSet)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+urlpatterns += router.urls
